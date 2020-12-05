a660bae7e86414d249ea912d6a56a486dec37164
mahir-d <mahirdhall4@gmail.com>
2020-12-05 14:31:12 -0500

    Styled badge shifed right

--- src/Components/StyledBadge.js
+++ src/Components/StyledBadge.js
@@ -5,67 +5,67 @@ import { makeStyles, withStyles } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
 
 const StyledBadge = withStyles((theme) => ({
-  badge: {
-    backgroundColor: "#44b700",
-    color: "##f50157",
-    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-    "&::after": {
-      position: "absolute",
-      top: 0,
-      left: 0,
-      width: "100%",
-      height: "100%",
-      borderRadius: "50%",
-      animation: "$ripple 1.2s infinite ease-in-out",
-      border: "1px solid currentColor",
-      content: '""'
-    }
-  },
-  "@keyframes ripple": {
-    "0%": {
-      transform: "scale(.8)",
-      opacity: 1
+    badge: {
+        backgroundColor: "#44b700",
+        color: "##f50157",
+        boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+        "&::after": {
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            borderRadius: "50%",
+            animation: "$ripple 1.2s infinite ease-in-out",
+            border: "1px solid currentColor",
+            content: '""'
+        }
     },
-    "100%": {
-      transform: "scale(2.4)",
-      opacity: 0
+    "@keyframes ripple": {
+        "0%": {
+            transform: "scale(.8)",
+            opacity: 1
+        },
+        "100%": {
+            transform: "scale(2.4)",
+            opacity: 0
+        }
     }
-  }
 }))(Badge);
 
 const SmallAvatar = withStyles((theme) => ({
-  root: {
-    width: 22,
-    height: 22,
-    border: `2px solid ${theme.palette.background.paper}`
-  }
+    root: {
+        width: 22,
+        height: 22,
+        border: `2px solid ${theme.palette.background.paper}`
+    }
 }))(Avatar);
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    "& > *": {
-      margin: theme.spacing(1)
+    root: {
+        display: "flex",
+        "& > *": {
+            margin: theme.spacing(1)
+        }
     }
-  }
 }));
 
-const BadgeAvatars =(props) =>{
-  const classes = useStyles();
+const BadgeAvatars = (props) => {
+    const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <StyledBadge
-        overlap="circle"
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "right"
-        }}
-        variant="dot"
+    return (
+        <div className={classes.root}>
+            <StyledBadge
+                overlap="circle"
+                anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "right"
+                }}
+                variant="dot"
 
-      >
-        <Avatar style={{width:"100px", height: "100px",color:"white" ,background: "#ed002a"}}><Typography noWrap>{props.data}</Typography></Avatar>
-      </StyledBadge>
-    </div>
-  );
+            >
+                <Avatar style={{ width: "100px", height: "100px", color: "white", background: "#ed002a", margin: "25px" }}><Typography noWrap>{props.data}</Typography></Avatar>
+            </StyledBadge>
+        </div>
+    );
 }
 export default BadgeAvatars
